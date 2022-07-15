ee44ede1c8d294bccb4bcbdc96032756e4992ccf
dobauron <safetysafetystrazak@gmail.com>
2022-07-15 16:17:57 +0200

    fighting with CLI

--- emailtask/emailtask.py
+++ emailtask/emailtask.py
@@ -11,24 +11,6 @@ def open_file(name):
     finally:
         f.close()
 
-
-# with open_file('/home/dob/PycharmProjects/emailtask - dobromir matuszak/'
-#                'recruitment-task-backend-internship-main/'
-#                'emails/email-pack-1.txt') as email_pack_1, \
-#         open_file('/home/dob/PycharmProjects/emailtask - dobromir matuszak/'
-#                   'recruitment-task-backend-internship-main/emails/emails-pack-2.txt') as email_pack_2, \
-#         open_file('/home/dob/PycharmProjects/emailtask - dobromir matuszak/'
-#                   'recruitment-task-backend-internship-main/emails/emails3.txt') as email_pack_3, \
-#         open_file('/home/dob/PycharmProjects/emailtask - dobromir matuszak/'
-#                   'recruitment-task-backend-internship-main/emails/other-emails4.txt') as email_pack_4, \
-#         open_file('/home/dob/PycharmProjects/emailtask - dobromir matuszak/'
-#                   'recruitment-task-backend-internship-main/emails/last-email-pack.csv') as email_pack_5:
-#     email_pack_list = [email_pack_1, email_pack_2, email_pack_3, email_pack_4, email_pack_5]
-#     for email_pack in email_pack_list:
-#         for email in email_pack.readlines():
-#             print(email)
-
-
 class EmailCollector:
     def __init__(self):
         self.all_email = []
@@ -70,25 +52,7 @@ class EmailCollector:
             for logs_email in email_pack_6_logs:
                 self.data_from_logs_file.append(logs_email)
 
-    def clear_data_logsfile_to_obtain_email(self, file):
-        logs_email_list = []
-        for line in file:
 
-            email = line.split('\'')
-            logs_email_list.append(email[1])
-
-
-        return logs_email_list
-
-    def is_exist_logsemail_in_all_email(self):
-        message_not_send_to_email_list = []
-        logs_email = self.clear_data_logsfile_to_obtain_email(self.data_from_logs_file)
-        for email in self.correct_email_list:
-
-            if email not in logs_email:
-                message_not_send_to_email_list.append(email)
-
-        return sorted(message_not_send_to_email_list)
 
     def clear_files_from_whitespace(self):
         temporary_list = []
@@ -124,6 +88,7 @@ class EmailCollector:
     def search_value(self, phrase):
         for email in self.all_email:
             if phrase in email:
+                print(phrase)
                 print(email)
 
     def sort_by_domain(self):
@@ -138,50 +103,85 @@ class EmailCollector:
         for item, value in sorted(domain_dict.items()):
             ListToStr = '\n\t'.join(map(str, sorted(value)))
             print(f'Domain {item} ({len(value)}):\n\t{ListToStr}')
-        # self.all_email.sort()
+
+    def clear_data_logsfile_to_obtain_email(self, file):
+        logs_email_list = []
+        for line in file:
+
+            email = line.split('\'')
+            logs_email_list.append(email[1])
 
 
+        return logs_email_list
+
+    def is_exist_logsemail_in_all_email(self):
+        message_not_send_to_email_list = []
+        logs_email = self.clear_data_logsfile_to_obtain_email(self.data_from_logs_file)
+        for email in self.correct_email_list:
+
+            if email not in logs_email:
+                message_not_send_to_email_list.append(email)
+
+        return sorted(message_not_send_to_email_list)
 E = EmailCollector()
+import sys
 
-app = typer.Typer()
 
+if __name__ == '__main__':
+    print(E.is_exist_logsemail_in_all_email())
+
+# import argparse
 #
+# parser = argparse.ArgumentParser(description='Process some integers.')
+# # parser.add_argument('--incorrect-email', '-ic', required=False)
+# # parser.add_argument('--search', '-s', type =str)
+# # parser.add_argument('--group-by-domain', '-gbd')
+# # parser.add_argument('--find-emails-not-in-logs', '-feil')
 #
+# parser.add_argument('integers', metavar='N', type=int, nargs='+',
+#                     help='an integer for the accumulator')
+# parser.add_argument('--sum', dest='accumulate', action='store_const',
+#                     const=sum, default=max,
+#                     help='sum the integers (default: find the max)')
+# args = parser.parse_args()
+# print(args)
+# parser = argparse.ArgumentParser()
+#
+# parser.add_argument("filename", help="path to a file to process")
+# parser.add_argument(
+#     "-s", "--search",
+# )
+# app = typer.Typer()
+
 # def main(incorrect_email: bool = typer.Option(...), search: str = typer.Option(...)):
 #     if search:
 #         print(E.search_value(search))
 #     if incorrect_email is True:
 #         for email in E.incorrect_email_list:
 #             print(email)
-
-@app.command('gbd')
-def gbd_email():
-    E.sort_by_domain()
-
-@app.command('ir')
-def email():
-    for email in E.incorrect_email_list:
-        print(email)
-
-
-@app.command('s')
-def email(
-        search: str = typer.Option(...)
-):
-    print(E.search_value(search))
-
-@app.command('feil')
-def email_logsfile():
-    for email in E.is_exist_logsemail_in_all_email():
-        print(email)
-
-
-
-
-
-
-if __name__ == "__main__":
-    app()
+#
+# @app.command('gbd')
+# def gbd_email():
+#     E.sort_by_domain()
+#
+# @app.command('ic')
+# def email():
+#     for email in E.incorrect_email_list:
+#         print(email)
+#
+#
+# @app.command('s')
+# def email(
+#         search: str = typer.Option(...)
+# ):
+#     print(E.search_value(search))
+#
+# @app.command('feil')
+# def email_logsfile():
+#     for email in E.is_exist_logsemail_in_all_email():
+#         print(email)
+# if __name__ == "__main__":
+#     app()
 
 # class File:
 #     def __init__(self, file_name, method):
